3f547e87da9f09d7be7b9b6d871b8eab5bf4a751
Sy Rashid <sy.rashid.11@gmail.com>
2022-03-01 11:24:59 +0000

    FEAT: Add inline form edit with stimulus

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -3,14 +3,6 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["list", "form", "input"]
 
-  connect() {
-    console.log("Hey I am connected to the search movies controller! 🎉");
-    console.log(this);
-    console.log(this.listTarget);
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-  }
-
   update() {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, {headers: { "Accept": "text/plain"}})
